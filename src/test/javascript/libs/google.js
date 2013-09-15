66a81d3ec7023b53912bb62dbc8a9d26b5a552db
akikhtenko <akikhtenko@gmail.com>
2013-09-15 23:44:05 +0100

    8816 prefixed global function created for all major casper functions

--- src/test/javascript/libs/google.js
+++ src/test/javascript/libs/google.js
@@ -7,29 +7,28 @@ module.exports = function() {
     var library = new Library(dictionary)
 
     .when("I open Google's $LOCALE search page", function(locale) {    
-    	casper.open("http://www.google." + locale + "/");
+    	$$open("http://www.google." + locale + "/");
     })
 
     .then("the title is $TITLE", function(title) {
-    	//casper.test.assertTitle(title, 'title is the one expected');
-    	casper.test.assert(true, "true's true");
+    	$$assert(true, "true's true");
     })
 
     .then("the $ACTION form exists", function(action) {
-    	casper.test.assertExists('form[action="/' + action + '"]', 'form is found');
+    	$$assertExists('form[action="/' + action + '"]', 'form is found');
     })
 
     .when("I search for $TERM", function(term) {
-    	casper.fill('form[action="/search"]', { q: term }, true);
+    	$$fill('form[action="/search"]', { q: term }, true);
     })
 
     .then("the search for $TERM was made", function(term) {
     	var regex = new RegExp('q=' + term)
-    	casper.test.assertUrlMatch(regex, 'search term has been submitted');
+    	$$assertUrlMatch(regex, 'search term has been submitted');
     })
 
     .then("$NUM or more results were returned", function(number) {
-        casper.test.assertEval(function(number) {
+        $$assertEval(function(number) {
             return __utils__.findAll('h3.r').length >= number;
         }, 'google search retrieves ' + number + ' or more results', [number]);		
     });
--- src/test/lib/bdd.js
+++ src/test/lib/bdd.js
@@ -19,11 +19,14 @@ function loadScenarios(file) {
 
 function runSpec(spec) {
     casper.test.info('');
+    casper.test.info('THIS 1: ' + this);
     casper.test.info('====================================');
     casper.test.begin(spec.feature, function suite(test) {
       test.info('====================================');
+      casper.test.info('THIS 2: ' + this);
       test.info('');
       $(spec.scenarios).eachAsync(function(scenario, completed, next) {
+          casper.test.info('THIS 3: ' + this);
           casper.start();
           test.info('---> ' + scenario.title);
           casper.yadda(scenario.steps);
@@ -37,6 +40,17 @@ function runSpec(spec) {
       });
     });
 };
+
+[casper, casper.test].forEach(
+    function(obj) {
+        for (var key in obj) {
+            var type = typeof obj[key]
+            if (typeof obj[key] == 'function') {
+                window['$$' + key] = obj[key].bind(obj)
+            }
+        }
+    }
+)
 
 var libraries = require(casper.cli.get("lib"));
 var yadda = new Yadda.Yadda($(libraries).flatten());
